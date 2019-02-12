e2ebf41ebca1aeec96aa319a276dcb13524e4b12
abutnal <45108373+abutnal@users.noreply.github.com>
2019-02-12 12:39:31 +0530

    Add files via upload

--- jscript.php
+++ jscript.php
@@ -196,7 +196,7 @@ $(document).ready(function(){
 	$("#input-fname").keypress(function(event){
 		var inputValue = event.which;
         // allow letters and whitespaces only.
-        if(!(inputValue >= 65 && inputValue <= 120) && (inputValue != 32 && inputValue != 0)) { 
+        if(!(inputValue >= 65 && inputValue <= 122) && (inputValue != 32 && inputValue != 0)) { 
         	event.preventDefault(); 
         }
     });
@@ -207,7 +207,7 @@ $(document).ready(function(){
 	$("#input-lname").keypress(function(event){
 		var inputValue = event.which;
         // allow letters and whitespaces only.
-        if(!(inputValue >= 65 && inputValue <= 120) && (inputValue != 32 && inputValue != 0)) { 
+        if(!(inputValue >= 65 && inputValue <= 122) && (inputValue != 32 && inputValue != 0)) { 
         	event.preventDefault(); 
         }
     });
